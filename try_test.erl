d071bb23a93ba680b3b2b74f81eb5e12b0a65dff
Ankit Goel <ankitgoel616@gmail.com>
2019-03-02 17:22:47 +0530

    Tested the catch statement

--- try_test.erl
+++ try_test.erl
@@ -1,5 +1,5 @@
 -module(try_test).
--export([demo1/0]).
+-export([demo1/0, demo2/0]).
 
 generate_exception(1) -> a;
 generate_exception(2) -> throw(a);
@@ -8,6 +8,7 @@ generate_exception(4) -> {'EXIT', a};
 generate_exception(5) -> error(a).
 
 demo1() -> [catcher(I) || I <- [1,2,3,4,5]].
+demo2() -> [{I, (catch generate_exception(I))} || I <- [1,2,3,4,5]].
 
 catcher(N) ->
     try generate_exception(N) of
